d58fc8fa255ae8da3066fc916d16a8d3dc87f711
Uppala Kishore <nagakishore64@gmail.com>
2024-05-25 17:33:47 +0530

    changes: create home,about pages and configure App.js with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-function Hi() {
-  // In browser->Inspect->Debugger->Browsers pause execution when they hit the debugger
-  // debugger;
-  return <p>Hello World</p>;
-}
+// function Hi() {
+//   // In browser->Inspect->Debugger->Browsers pause execution when they hit the debugger
+//   // debugger;
+//   return <p>Hello World</p>;
+// }
 
-const hi = render(<Hi />, document.getElementById("app"));
+// const hi = render(<Hi />, document.getElementById("app"));
+// export default hi;
 
-export default hi;
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
