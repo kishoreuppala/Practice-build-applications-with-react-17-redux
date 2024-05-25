7fd16bcf2b60f11c60a690abc9a142e7d4be6cee
Uppala Kishore <nagakishore64@gmail.com>
2024-05-25 22:53:59 +0530

    changes: Course page draft created & Ready for Redux

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//working with class component
+class CoursesPage extends React.Component {
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+
+export default CoursesPage;
